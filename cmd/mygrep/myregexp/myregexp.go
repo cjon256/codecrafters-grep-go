0ce955bf65a38d4ba15b4049c6de4cac3e86c049
Cullen J. ONeill <3659487+cjon256@users.noreply.github.com>
2025-01-14 22:40:12 -0500

    redesign of the + feature with proper backtracking
    kinda messed up by initial design doing this, will probably need to
    refactor before taking on anything that adds substantial functionality

--- cmd/mygrep/myregexp/myregexp.go
+++ cmd/mygrep/myregexp/myregexp.go
@@ -35,96 +35,110 @@ type matchPoint struct {
 	wildtype   wildcard
 }
 
-func (re MyRegExp) matchHere(line []byte, current int) bool {
-	for i := 0; i < len(re.mps); i++ {
-		if current >= len(line) {
-			fmt.Fprintln(os.Stderr, "at the end...")
-			if re.mps[i].wildtype == zeroOrOne {
-				continue
-			}
-			fmt.Fprintln(os.Stderr, "oops, got to long")
-			return false
+func (mp matchPoint) matchByte(c byte) bool {
+	matches := strings.Contains(mp.matchChars, string(c))
+	if mp.inverted {
+		matches = !matches
+	}
+	return matches
+}
+
+func (re MyRegExp) matchHere(line []byte, current int, rdx int) bool {
+	if rdx >= len(re.mps) {
+		// so we're at the end of the regexp
+		// we return true unless there was a $ at the end... if that is the case
+		// then we need to check we are at the end of the input
+		if !re.matchEnd {
+			return true
 		}
-		if !re.mps[i].match(line, &current) {
+		// ah, matchEnd was set so we succeed if at end and fail if not...
+		atEnd := current == len(line)
+		if !atEnd {
 			fmt.Fprintln(os.Stderr, "match fails in regExp::matchHere")
-			return false
 		}
+		return atEnd
 	}
-	if re.matchEnd && current != len(line) {
-		fmt.Fprintf(os.Stderr, "Not at end despite match otherwise\n")
+	mp := re.mps[rdx]
+	fmt.Fprintf(os.Stderr, "matchHere(line='%s', current=%d, rdx=%d) with mp = '%v+'\n", string(line)[current:], current, rdx, mp)
+	if current >= len(line) {
+		fmt.Fprintln(os.Stderr, "at the end...")
+		// ah, we're at the end of the string but not the end of the regexp
+		// but we might still match if all the remaining items can be zero length
+		if mp.wildtype == zeroOrOne {
+			return re.matchHere(line, current, rdx+1)
+		}
+		fmt.Fprintln(os.Stderr, "oops, got to long")
 		return false
 	}
-
-	return true
-}
-
-func (mp *matchPoint) matchOnce(line []byte, current *int) bool {
-	matches := strings.Contains(mp.matchChars, string((line)[*current]))
-	fmt.Fprintf(os.Stderr, "matchOnce(line='%s', current=%d) with mp = '%v+'\n", string(line), *current, mp)
-	if mp.inverted {
-		matches = !matches
-	}
-	if matches {
-		// fmt.Fprintln("matches")
-		*current++
-		return true
-	} else {
-		// fmt.Fprintln("fails")
-		return false
-	}
-}
-
-func (mp *matchPoint) matchZeroOrOne(line []byte, current *int) bool {
-	matches := strings.Contains(mp.matchChars, string((line)[*current]))
-	fmt.Fprintf(os.Stderr, "matchZeroOrOne(line='%s', current=%d) with mp = '%v+'\n", string(line), *current, mp)
-	if mp.inverted {
-		matches = !matches
-	}
-	if matches {
-		// fmt.Fprintln("matches")
-		*current++
-		return true
-	} else {
-		// fmt.Fprintln("fails")
-		return true
-	}
-}
-
-func (mp *matchPoint) matchOneOrMore(line []byte, current *int) bool {
-	overallMatch := false
-	count := 0
-	for {
-		matches := strings.Contains(mp.matchChars, string((line)[*current]))
-		fmt.Fprintf(os.Stderr, "matchOneOrMore(line='%s', current=%d) with mp = '%v+'\n", string(line)[*current:], *current, mp)
-		if mp.inverted {
-			matches = !matches
-		}
-		if matches {
-			count++
-			fmt.Fprintf(os.Stderr, "%d matches\n", count)
-			*current++
-			overallMatch = true
-		} else {
-			fmt.Fprintf(os.Stderr, "End with %d matches\n", count)
-
-			return overallMatch
-		}
-		if *current >= len(line) {
-			return overallMatch
-		}
-	}
-}
-
-func (mp matchPoint) match(line []byte, current *int) bool {
 	switch mp.wildtype {
 	case oneOrMore:
-		return mp.matchOneOrMore(line, current)
+		return re.matchOneOrMore(line, current, rdx)
 	case zeroOrOne:
-		return mp.matchZeroOrOne(line, current)
+		return re.matchZeroOrOne(line, current, rdx)
 	default:
-		return mp.matchOnce(line, current)
+		if !mp.matchByte(line[current]) {
+			return false
+		}
+		return re.matchHere(line, current+1, rdx+1)
 	}
 }
+
+func (re MyRegExp) matchZeroOrOne(line []byte, current int, rdx int) bool {
+	mp := re.mps[rdx]
+	if !mp.matchByte(line[current]) {
+		return re.matchHere(line, current, rdx+1)
+	}
+	return re.matchHere(line, current+1, rdx+1)
+}
+
+func (re MyRegExp) matchOneOrMore(line []byte, current int, rdx int) bool {
+	mp := re.mps[rdx]
+	fmt.Fprintf(os.Stderr, "matchOneOrMore(line='%s', current=%d, rdx=%d) with mp = '%v+'\n", string(line)[current:], current, rdx, mp)
+	if !mp.matchByte(line[current]) {
+		return false
+	}
+	// finding max length that will match and then working backwards
+	maxLength := 1
+	for ; maxLength+current < len(line); maxLength++ {
+		if !mp.matchByte(line[maxLength+current]) {
+			// keep incrementing until we fail to match or hit the end
+			break
+		}
+	}
+	fmt.Fprintf(os.Stderr, "maxLength: %d\n", maxLength)
+	// here is the working backwards
+	for trialLength := maxLength; trialLength > 0; trialLength-- {
+		fmt.Fprintf(os.Stderr, "trialLength: %d\n", trialLength)
+		if re.matchHere(line, current+trialLength, rdx+1) {
+			return true
+		}
+	}
+	return false
+}
+
+// 	overallMatch := false
+// 	count := 0
+// 	for {
+// 		matches := strings.Contains(mp.matchChars, string((line)[*current]))
+// 		fmt.Fprintf(os.Stderr, "matchOneOrMore(line='%s', current=%d) with mp = '%v+'\n", string(line)[*current:], *current, mp)
+// 		if mp.inverted {
+// 			matches = !matches
+// 		}
+// 		if matches {
+// 			count++
+// 			fmt.Fprintf(os.Stderr, "%d matches\n", count)
+// 			*current++
+// 			overallMatch = true
+// 		} else {
+// 			fmt.Fprintf(os.Stderr, "End with %d matches\n", count)
+//
+// 			return overallMatch
+// 		}
+// 		if *current >= len(line) {
+// 			return overallMatch
+// 		}
+// 	}
+// }
 
 func parseSetPattern(inverted bool, pattern *string, index *int) (matchPoint, error) {
 	retval := matchPoint{}
@@ -228,7 +242,7 @@ func (re *MyRegExp) MatchLine(line []byte) (bool, error) {
 		limit = 1
 	}
 	for current := 0; current < limit; current++ {
-		matchesHere := re.matchHere(line, current)
+		matchesHere := re.matchHere(line, current, 0)
 		if matchesHere {
 			fmt.Fprintln(os.Stderr, "ml whole matched")
 			return true, nil
